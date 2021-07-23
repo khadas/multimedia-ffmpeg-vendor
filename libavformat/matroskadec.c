d0161921288d178c766f398e378de25f5ccfd599
chao.xiong <chao.xiong@amlogic.com>
2021-07-23 16:46:35 +0800

    ffmpeg-aml: Fix the error of cts. [2/2]
    PD#SWPL-55246
    
    Problem:
    android.media.cts.DecoderTest#testTrackSelectionMkv fail.
    
    Solution:
    Add the audio first key frame entries despite it's pts below zero.
    
    Verify:
    AndroidR
    
    Change-Id: I89306667cc66bd98d119d515f5dc4c133f8c0f90
    Signed-off-by: chao.xiong@amlogic.com

--- libavformat/matroskadec.c
+++ libavformat/matroskadec.c
@@ -3337,7 +3337,8 @@ static int matroska_parse_block(MatroskaDemuxContext *matroska, uint8_t *data,
         is_keyframe = flags & 0x80 ? AV_PKT_FLAG_KEY : 0;
 
     if (cluster_time != (uint64_t) -1 &&
-        (block_time >= 0 || cluster_time >= -block_time)) {
+        (block_time >= 0 || cluster_time >= -block_time ||
+        (cluster_time < -block_time && st->nb_index_entries == 0 && track->type == MATROSKA_TRACK_TYPE_AUDIO))) {
         timecode = cluster_time + block_time - track->codec_delay_in_track_tb;
         if (track->type == MATROSKA_TRACK_TYPE_SUBTITLE &&
             timecode < track->end_timecode)
