22950c559d28d1080bf249f3a4e565067493ff90
Qiang Guo <qiang.guo@amlogic.com>
2023-12-27 14:16:33 +0800

    ffmpeg-aml: CB2 special h264 no keyframe cause playback fail [1/1]
    PD#SWPL-151062
    
    Problem:
    special h264 no keyframe cause playback fail.
    
    Solution:
    if the I-frame contains SPS and PPS data, it is
    considered a key frame.
    
    Verify:
    Android U OHM
    
    Change-Id: I73115323229d0989e01f996caf1aa6379c05f32e
    Signed-off-by: Qiang Guo <qiang.guo@amlogic.com>

--- libavcodec/h264_parser.c
+++ libavcodec/h264_parser.c
@@ -251,6 +251,7 @@ static inline int parse_nal_units(AVCodecParserContext *s,
     int q264 = buf_size >=4 && !memcmp("Q264", buf, 4);
     int field_poc[2];
     int ret;
+    int has_sps = 0, has_pps = 0;
 
     /* set some sane default values */
     s->pict_type         = AV_PICTURE_TYPE_I;
@@ -316,9 +317,11 @@ static inline int parse_nal_units(AVCodecParserContext *s,
 
         switch (nal.type) {
         case H264_NAL_SPS:
+            has_sps = 1;
             ff_h264_decode_seq_parameter_set(&nal.gb, avctx, &p->ps, 0);
             break;
         case H264_NAL_PPS:
+            has_pps = 1;
             ff_h264_decode_picture_parameter_set(&nal.gb, avctx, &p->ps,
                                                  nal.size_bits);
             break;
@@ -395,6 +398,10 @@ static inline int parse_nal_units(AVCodecParserContext *s,
             // heuristic to detect non marked keyframes
             if (p->ps.sps->ref_frame_count <= 1 && p->ps.pps->ref_count[0] <= 1 && s->pict_type == AV_PICTURE_TYPE_I)
                 s->key_frame = 1;
+
+            if (!s->key_frame && has_sps && has_pps && s->pict_type == AV_PICTURE_TYPE_I) {
+                s->key_frame = 1;
+            }
 
             p->poc.frame_num = get_bits(&nal.gb, sps->log2_max_frame_num);
 
